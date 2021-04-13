7d2184b1ca30f20358a07433eda01caa1aa55eb5
seunghun <tmdgnsdl23@naver.com>
2021-04-13 22:35:23 +0900

    +Label / Button

---
+++ TotalProject/BountyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BountyViewController.swift
+//  TotalProject
+//
+//  Created by 오승훈 on 2021/04/13.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ TotalProject/musicViewController.swift
@@ -0,0 +1,18 @@
+//
+//  musicViewController.swift
+//  TotalProject
+//
+//  Created by 오승훈 on 2021/04/13.
+//
+
+import UIKit
+
+class musicViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
